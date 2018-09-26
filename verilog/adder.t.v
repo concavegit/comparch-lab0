308133bf0ed56fe7e6970a62f55989f16172fcf1
Adam Selker <adamselker@gmail.com>
2018-09-25 21:47:12 -0400

    First draft of better testbench

--- verilog/adder.t.v
+++ verilog/adder.t.v
@@ -12,92 +12,82 @@ module fulladder4bit_tb ();
 
    FullAdder4bit add(sum, carryout, overflow, a, b);
 
+	 //reg signed [15:0] testA[3:0], testB[3:0], testSum[3:0];
+	 reg signed [3:0] testA[15:0], testB[15:0], testSum[15:0];
+	 reg [15:0] testCarryout, testOverflow;
+
+
+	 integer ii;
+
    initial begin
       $dumpfile("test.vcd");
       $dumpvars();
-      $monitor("%d %d %d %d %d", a, b, sum, carryout, overflow);
+      //$monitor("%d %d %d %d %d", a, b, sum, carryout, overflow);
 
-      // 5 + 3 nonnegative inputs, sum nonnegative, overflow, no carry
-      a = 4'b0101;
-      b = 4'b0011;
-      #600
+			// As far as I know, there is no better way to do this in non-System Verilog.
 
-        // 4 + 1 both nonegative inputs, sum nonnegative, no overflow, no carry
-        a = 4'b0100;
-      b = 4'b0001;
-      #600
+			testA[0] = 4'b0101;
+			testA[1] = 4'b0100;
+			testA[2] = 4'b0010;
+			testA[3] = 4'b0110;
+			testA[4] = 4'b1110;
+			testA[5] = 4'b1011;
+			testA[6] = 4'b1000;
+			testA[7] = 4'b1100;
+			testA[8] = 4'b0111;
+			testA[9] = 4'b0111;
+			testA[10] = 4'b0111;
+			testA[11] = 4'b0000;
+			testA[12] = 4'b1000;
+			testA[13] = 4'b0000;
+			testA[14] = 4'b0000;
+			testA[15] = 4'b1111;
 
-        // 2 - 1 nonnegative and negative, sum nonnegative, no overflow, carry
-        a = 4'b0010;
-      b = 4'b1111;
-      #600
+			testB[0] = 4'b0011;
+			testB[1] = 4'b0001;
+			testB[2] = 4'b1111;
+			testB[3] = 4'b1001;
+			testB[4] = 4'b0111;
+			testB[5] = 4'b0000;
+			testB[6] = 4'b1010;
+			testB[7] = 4'b1101;
+			testB[8] = 4'b0111;
+			testB[9] = 4'b0000;
+			testB[10] = 4'b1000;
+			testB[11] = 4'b1000;
+			testB[12] = 4'b1000;
+			testB[13] = 4'b1111;
+			testB[14] = 4'b0000;
+			testB[15] = 4'b1111;
 
-        // 6 - 7 nonnegative and negative, sum negative, no overflow, no carry
-        a = 4'b0110;
-      b = 4'b1001;
-      #600
+			testSum[0] = 4'b0000;
+			testSum[1] = 4'b0000;
+			testSum[2] = 4'b0000;
+			testSum[3] = 4'b0000;
+			testSum[4] = 4'b0000;
+			testSum[5] = 4'b0000;
+			testSum[6] = 4'b0000;
+			testSum[7] = 4'b0000;
+			testSum[8] = 4'b0000;
+			testSum[9] = 4'b0000;
+			testSum[10] = 4'b0000;
+			testSum[11] = 4'b0000;
+			testSum[12] = 4'b0000;
+			testSum[13] = 4'b0000;
+			testSum[14] = 4'b0000;
+			testSum[15] = 4'b0000;
 
-        // -2 + 7 negative and nonnegative inputs, sum nonnegative, no
-        // overflow, carry
-        a = 4'b1110;
-      b = 4'b0111;
-      #600 
+			for (ii = 0; ii < 16; ii = ii+1) begin
+				a = testA[ii];
+				b = testB[ii];
+				# 600
+				if (sum != testSum[ii]) 
+					$display("oh no");
+				else
+					$display("ok");
+			end
 
-        // -5 + 0 negative and nonnegative inputs, sum negative, no
-        // overflow, no carry
-        a = 4'b1011;
-      b = 4'b0000;
-      #600
 
-        // -8 - 6 negative inputs, sum negative, overflow, carry
-        a = 4'b1000;
-      b = 4'b1010;
-      #600
-
-        // -4 - 3 negative inputs, sum negative, no overflow, carry
-        a = 4'b1100;
-      b = 4'b1101;
-      #600
-
-        // 7 + 7 extreme
-        a = 4'b0111;
-      b = 4'b0111;
-      #600
-
-        // 7 + 0 extreme
-        a = 4'b0111;
-      b = 4'b0000;
-      #600
-
-        // 7 - 8 extreme
-        a = 4'b0111;
-      b = 4'b1000;
-      #600
-
-        // 0 - 8 extreme
-        a = 4'b0000;
-      b = 4'b1000;
-      #600
-
-        // -8 - 8 extreme
-        a = 4'b1000;
-      b = 4'b1000;
-      #600
-
-        // 0 - 1 extreme
-        a = 4'b0000;
-      b = 4'b1111;
-      #600
-
-        // 0 + 0 extreme/worst-case delay
-        a = 4'b0000;
-      b = 4'b0000;
-      #600
-
-        // -1 - 1 extreme/wost-case delay
-        a = 4'b1111;
-      b = 4'b1111;
-      #600
       $finish;
    end
 endmodule
